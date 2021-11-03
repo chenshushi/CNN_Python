30d02d870d73f9dc7a85472aa173987f631174cc
chenss <chenshushi98@163.com>
2021-11-03 11:41:04 +0800

    a. begin point

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .AlexNet import AlexNet
+from .AlexNet import Q_AlexNet
+from .ResNet34 import ResNet34
+# from .PYNQ_net import PYNQ_net
+# from torchvision.models import InceptinV3
+# from torchvision.models import alexnet as AlexNet
